0f305af8a1e6af2282be5a465c9cb0366fad832c
Ekaterina Davliatchina <flykat@yandex.ru>
2017-06-03 01:00:16 +1000

    added scroll to blocks

--- js/script.js
+++ js/script.js
@@ -273,3 +273,11 @@ $('.results__items').magnificPopup({
   gallery:{enabled:true}
 });
 
+
+$("a.scrollto").click(function() {
+var elementClick = $(this).attr("href")
+var destination = $(elementClick).offset().top;
+jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+return false
+});
+
